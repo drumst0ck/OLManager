35ed178829f89b89fe9b178c5aed6dec902e8d08
Alejandro Alonso Lopez <aalonsopersonal@gmail.com>
2026-05-07 01:28:39 +0200

    fix(settings): added dynamic version and changed author

---
+++ src/lib/appInfo.ts
@@ -0,0 +1,6 @@
+// App constants - version from package.json (0.1.2)
+// This file is automatically updated by the release process
+// Do not edit manually
+
+export const APP_VERSION = "0.1.2";
+export const APP_NAME = "openleaguemanager";
